7af01c8a77c1ca48c5b5e0550d15d36d23bd54ba
Carsten Zimmermann <cz@aegisnet.de>
2011-04-16 12:47:10 +0200

    Moved create_invalid_xml_response and stub_valid_xml_api_response! into test helper

--- test/unit/google_weather_test.rb
+++ test/unit/google_weather_test.rb
@@ -83,55 +83,6 @@ class GoogleWeatherTest < ActiveSupport::TestCase
     assert_equal File.join(Regentanz.configuration.cache_dir, "#{Regentanz.configuration.cache_prefix}_foo.xml"), obj.cache_filename
   end
 
-  protected
-
-  def create_invalid_xml_response(filename)
-    File.open(filename, "w+") do |f|
-      f.write '<HTML><HEAD><meta http-equiv="content-type" content="text/html;charset=utf-8">
-<TITLE>302 Moved</TITLE></HEAD><BODY>
-<H1>302 Moved</H1>
-The document has moved
-<A HREF="http://sorry.google.com/sorry/?continue=http://www.google.com/ig/api%3Fweather%3D86747%252CGermany%26hl%3Dde">here</A>.
-</BODY></HTML>
-'
-    end
-  end
-
-  # Stub Net::HTTP.get_response to return a semi-dynamic (ie. current date) xml
-  # response-
-  def stub_valid_xml_api_response!
-    xml_templ = ERB.new <<-EOF
-<?xml version="1.0"?>
-<xml_api_reply version="1">
-    <weather module_id="0" tab_id="0" mobile_row="0" mobile_zipped="1" row="0" section="0" >
-        <forecast_information>
-            <city data="Berlin, Berlin"/><postal_code data=Berlin,Germany"/>
-            <latitude_e6 data=""/>
-            <longitude_e6 data=""/>
-            <forecast_date data="<%= Date.today.strftime("%Y-%m-%d") %>"/>
-            <current_date_time data="<%= 5.minutes.ago.utc.strftime("%Y-%m-%d %H:%M:00") %> +0000"/>
-            <unit_system data="SI"/>
-        </forecast_information>
-        <current_conditions>
-            <condition data="Klar"/><temp_f data="77"/>
-            <temp_c data="25"/>
-            <humidity data="Feuchtigkeit: 57 %"/>
-            <icon data="/ig/images/weather/sunny.gif"/>
-            <wind_condition data="Wind: W mit 24 km/h"/>
-        </current_conditions>
-        <forecast_conditions>
-            <day_of_week data="Do."/><low data="16"/>
-            <high data="31"/>
-            <icon data="/ig/images/weather/chance_of_rain.gif"/>
-            <condition data="Vereinzelt Regen"/>
-        </forecast_conditions>
-    </weather>
-</xml_api_reply>
-EOF
-    mock_response = mock()
-    mock_response.stubs(:body).returns(xml_templ.result)
-    Net::HTTP.stubs(:get_response).returns(mock_response)
-  end
 
   # Return a few default options for the test environment
   def weather_options(options = {})
