04fd363e9549f431c80303c292bd030fe92bb44b
Carsten Zimmermann <carp@hacksocke.de>
2013-06-09 22:27:33 +0200

    Remove actionmailer in test setup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,14 +1,10 @@
-require "rubygems"
+require 'rubygems'
 require "bundler/setup"
 
 require "test/unit"
 require "active_support/test_case"
-require 'action_mailer/test_helper'
 Bundler.require(:default, :development)
 require 'test/factories'
-
-# FIXME move SupportMailer into callbacks
-require File.join(File.dirname(__FILE__), 'support', 'support_mailer')
 
 # Configure for test mode
 require 'regentanz/test_helper'
